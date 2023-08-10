2160660cc25f2b9527b62f2d166dcdbb22f1f7ec
Francesca <francescafacia@gmail.com>
2023-08-10 17:24:23 -0300

    agregando conexion a la base de datos

---
+++ db/mongodb.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+// conectar a la base de datos
+
+const connectDb = async () => {
+  try {
+    await mongoose.connect("mongodb://0.0.0.0:27017/juegafulbo", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Conectado a la base de datos");
+  } catch (error) {
+    console.log("Error al conectar", error);
+  }
+};
+
+module.exports = connectDb;
---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express"); // importar express
+const app = express(); // inicializar express
+const connectDb = require("./db/mongodb");
+// const comprabacionJwt = require("./middleware/comprobacionjwt");
+
+app.use(express.json()); // permite recibir obj en forma de json
+app.use(express.urlencoded({ extended: true })); // permite recibir parametros y queris en la rutas
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3000;
+
+const initApp = async () => {
+  try {
+    app.listen(PORT, () => {
+      console.log(`Servidor puesto en marcha en el puerto ${PORT}`);
+    });
+    await connectDb();
+  } catch (error) {
+    console.log("Error al iniciar la aplicacion");
+  }
+};
+
+initApp();
+
+// app.use("/api", require("./routes/Rutas.Canchas"));
+// app.use("/api/user", require("./routes/Rutas.Usuarios"));
+// app.use("/protegida", comprabacionJwt, require("./routes/RutaAdmin"));
+
+// crear una ruta em express
+
+// app.use("/api", require("./routes/Rutas.Canchas"));
+// app.use("/api/user", require("./routes/Rutas.Usuarios"));
+// app.use("/protegida", comprabacionJwt, require("./routes/RutaAdmin"));
